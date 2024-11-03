b8e43267d9489d44cb7f2f8a48f32c6115e0b757
Aurora3001 <bero_22@alu.uabcs.mx>
2024-11-03 14:00:04 -0700

    agregar app lista y otras cosas

---
+++ my-expo-app/App.js
@@ -0,0 +1,26 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+import Header from './components/Header';
+import AddTask from './components/AddTask';
+import CheckboxContainer from './components/CheckboxContainer';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <StatusBar style="auto" />
+      <Header/>
+      <AddTask/>
+      <CheckboxContainer/>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    flexDirection: 'column',
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
